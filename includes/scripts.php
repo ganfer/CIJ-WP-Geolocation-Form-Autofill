af3a558a67aca34f82bb99a9492429d46c7058e8
ganfer <100222172+ganfer@users.noreply.github.com>
2024-11-14 08:54:18 +0100

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,20 +1,65 @@
 <?php
-// Register and enqueue scripts based on the selected embed location
-function gf_autofill_enqueue_script() {
-    if (get_option('gf_autofill_enabled')) {
-        $embed_location = get_option('gf_autofill_embed_location');
-        if ($embed_location === 'header') {
-            wp_enqueue_script('gf_autofill_script', GEOLOCATION_FORM_AUTOFILL_URL . 'js/geolocation.js', array(), '1.0', false);
-        }
-    }
-}
-add_action('wp_enqueue_scripts', 'gf_autofill_enqueue_script');
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-// Shortcode for selective embedding
-function gf_autofill_shortcode() {
-    if (get_option('gf_autofill_shortcode_enabled')) {
-        wp_enqueue_script('gf_autofill_script', GEOLOCATION_FORM_AUTOFILL_URL . 'js/geolocation.js', array(), '1.0', false);
+class GeolocationFormAutofillScripts {
+    public function __construct() {
+        if (get_option('geolocation_form_autofill_enable') && get_option('geolocation_form_autofill_location') === 'header') {
+            add_action('wp_head', array($this, 'add_geolocation_script'));
+        }
+
+        add_shortcode('geolocation_form_autofill', array($this, 'shortcode_geolocation_script'));
+    }
+
+    // JavaScript-Code in den Header einbinden
+    public function add_geolocation_script() {
+        echo $this->generate_geolocation_script();
+    }
+
+    // Shortcode für gezielte Einbindung des Scripts
+    public function shortcode_geolocation_script() {
+        return $this->generate_geolocation_script();
+    }
+
+    // Generierung des Geolocation-Scripts
+    private function generate_geolocation_script() {
+        ob_start();
+        ?>
+        <script>
+            document.addEventListener("d365mkt-afterformload", function() {
+                var cityField = document.querySelector("input[name='address1_city']");
+                var zipField = document.querySelector("input[name='address1_postalcode']");
+
+                if ("geolocation" in navigator && (cityField || zipField)) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        var latitude = position.coords.latitude;
+                        var longitude = position.coords.longitude;
+                        var geolocationApiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+
+                        fetch(geolocationApiUrl)
+                            .then(response => response.json())
+                            .then(data => {
+                                var city = data.address.city;
+                                var zip = data.address.postcode;
+                                console.log("Stadt: " + city);
+                                console.log("PLZ: " + zip);
+
+                                if (cityField) cityField.value = city;
+                                if (zipField) zipField.value = zip;
+                            })
+                            .catch(error => {
+                                console.error("Fehler bei der Geolokalisierung: " + error);
+                            });
+                    });
+                } else {
+                    console.log("Geolocation oder erforderliches Feld (city oder zip) ist nicht verfügbar.");
+                }
+            });
+        </script>
+        <?php
+        return ob_get_clean();
     }
 }
-add_shortcode('geolocation_form_autofill', 'gf_autofill_shortcode');
+
+// Initialisierung des Scripts
+new GeolocationFormAutofillScripts();
 ?>
